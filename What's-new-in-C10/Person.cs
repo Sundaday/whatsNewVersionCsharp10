3b3a0210c5a9a21f89888a3b84851eb4bf173ba4
Sundaday <john.koch.pro@gmail.com>
2022-03-20 22:19:55 +0100

    using global + structs

---
+++ What's-new-in-C10/Person.cs
@@ -0,0 +1,39 @@
+﻿//possibilité de mettre un
+//namespace What_s_new_in_C10; et retirer un niveau d'indentation
+//mais on ne pourra plus utiliser plusieurs namespace au sein d'une meme classe
+using Newtonsoft.Json;
+
+namespace What_s_new_in_C10
+{
+    class Person
+    {
+        public string? name { get; set; } //string? = string OR nullable
+        public int age { get; set; }
+
+        public string GetJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        //Console.WriteLine("Hello, World!");
+
+        //var p = new Person { name = "toto", age = 5 };
+        //var json_p = JsonConvert.SerializeObject(p);
+        //Console.WriteLine(json_p);
+        //Console.WriteLine(p.GetJson());
+    }
+}
+//pratique extremement rare
+namespace What_s_new_in_C102
+{
+    class Person
+    {
+        public string? name { get; set; } //string? = string OR nullable
+        public int age { get; set; }
+
+        public string GetJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ What's-new-in-C10/Program.cs
@@ -0,0 +1,13 @@
+﻿// C# 10 - GLOBAL USING
+//global using Newtonsoft.Json; //Portée globale du newtonsoft = accessible dans la class person
+
+namespace MyApp // Note actual namespace depends on the project name
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
