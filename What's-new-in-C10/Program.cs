cc3382ea15e6335e720ccd933352268390935600
Sundaday <john.koch.pro@gmail.com>
2022-03-21 02:03:33 +0100

    struct

--- What's-new-in-C10/Program.cs
+++ What's-new-in-C10/Program.cs
@@ -1,13 +1,21 @@
 ﻿// C# 10 - GLOBAL USING
 //global using Newtonsoft.Json; //Portée globale du newtonsoft = accessible dans la class person
 
-namespace MyApp // Note actual namespace depends on the project name
+//Struct C#10 
+namespace cs10_structs 
 {
+
+    struct PersonStruct
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+    }
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            var p = new PersonStruct() { name = "toto", id = 5 };
+            Console.WriteLine(p);
         }
     }
 }
